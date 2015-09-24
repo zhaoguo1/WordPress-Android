faf36eaa079a168082796049c714851a82b5b571
Nick Bradbury <nick.bradbury@gmail.com>
2015-09-24 13:47:23 -0400

    Second pass at CustomTabsManager

--- WordPress/src/main/java/org/wordpress/android/util/CustomTabsManager.java
+++ WordPress/src/main/java/org/wordpress/android/util/CustomTabsManager.java
@@ -2,21 +2,19 @@ package org.wordpress.android.util;
 
 
 import android.app.Activity;
-import android.app.ActivityOptions;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.customtabs.CustomTabsServiceConnection;
 import android.support.customtabs.CustomTabsSession;
 import android.text.TextUtils;
 
+import org.wordpress.android.R;
 import org.wordpress.android.ui.WPWebViewActivity;
 import org.wordpress.passcodelock.AppLockManager;
 
@@ -42,79 +40,15 @@ public class CustomTabsManager {
         return new CustomTabsManager();
     }
 
-    private static Boolean mIsCustomTabsSupported;
-    private static boolean isCustomTabsSupported(Context context) {
-        if (mIsCustomTabsSupported == null) {
-            String packageNameToBind = CustomTabsHelper.getPackageNameToUse(context);
-            if (packageNameToBind == null) {
-                mIsCustomTabsSupported = false;
-                return false;
-            }
-
-            CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
-                @Override
-                public void onCustomTabsServiceConnected(ComponentName name, CustomTabsClient client) {
-                }
-                @Override
-                public void onServiceDisconnected(ComponentName name) {
-                }
-            };
-
-            mIsCustomTabsSupported = CustomTabsClient.bindCustomTabsService(context, packageNameToBind, connection);
-            if (mIsCustomTabsSupported) {
-                context.unbindService(connection);
-            }
-        }
-
-        return mIsCustomTabsSupported;
-    }
-
-
     /*
      * use these two routines to open a URL when a CustomTabsManager isn't necessary
      */
     public static void browseUrl(Context context, String url) {
-        browseUrl(context, url, OpenUrlType.INTERNAL);
+        newInstance().openUrl(context, url);
     }
 
     public static void browseUrl(Context context, String url, OpenUrlType openUrlType) {
-        if (openUrlType == OpenUrlType.EXTERNAL) {
-            browseUrlExternal(context, url);
-        } else if (openUrlType == OpenUrlType.INTERNAL && !isCustomTabsSupported(context)) {
-            WPWebViewActivity.openURL(context, url);
-        } else {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            intent.putExtra(CustomTabsIntent.EXTRA_TITLE_VISIBILITY_STATE, CustomTabsIntent.SHOW_PAGE_TITLE);
-            CustomTabsHelper.addKeepAliveExtra(context, intent);
-
-            Bundle extras = new Bundle();
-            extras.putBinder(CustomTabsIntent.EXTRA_SESSION, null);
-            intent.putExtras(extras);
-
-            Bitmap icon = BitmapFactory.decodeResource(
-                    context.getResources(), R.drawable.ic_arrow_back_black_24dp);
-            intent.putExtra(CustomTabsIntent.EXTRA_CLOSE_BUTTON_ICON, icon);
-
-            Bundle finishBundle = ActivityOptions.makeCustomAnimation(
-                    context, R.anim.do_nothing, R.anim.activity_slide_out_to_right).toBundle();
-            intent.putExtra(CustomTabsIntent.EXTRA_EXIT_ANIMATION_BUNDLE, finishBundle);
-
-            Bundle startBundle = ActivityOptions.makeCustomAnimation(
-                    context, R.anim.activity_slide_in_from_right, R.anim.do_nothing).toBundle();
-
-            context.startActivity(intent, startBundle);
-        }
-    }
-
-    private static void browseUrlExternal(Context context, String url) {
-        try {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            context.startActivity(intent);
-            AppLockManager.getInstance().setExtendedTimeout();
-        } catch (ActivityNotFoundException e) {
-            String errString = context.getString(org.wordpress.android.R.string.reader_toast_err_url_intent);
-            ToastUtils.showToast(context, String.format(errString, url), ToastUtils.Duration.LONG);
-        }
+        newInstance().openUrl(context, url, openUrlType);
     }
 
     private CustomTabsManager() {
@@ -168,8 +102,16 @@ public class CustomTabsManager {
         if (context == null || TextUtils.isEmpty(url)) return;
 
         if (openUrlType == OpenUrlType.EXTERNAL) {
-            browseUrlExternal(context, url);
-        } else if (mCustomTabsSession != null && context instanceof Activity) {
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                context.startActivity(intent);
+                AppLockManager.getInstance().setExtendedTimeout();
+            } catch (ActivityNotFoundException e) {
+                String errString = context.getString(org.wordpress.android.R.string.reader_toast_err_url_intent);
+                ToastUtils.showToast(context, String.format(errString, url), ToastUtils.Duration.LONG);
+            }
+        } else if (isCustomTabsSupported(context)) {
+            // note that it's okay for mCustomTabsSession to be null here
             CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder(mCustomTabsSession);
             builder.setShowTitle(true);
             builder.setStartAnimations(context, R.anim.activity_slide_in_from_right, R.anim.do_nothing);
@@ -179,8 +121,57 @@ public class CustomTabsManager {
             CustomTabsIntent customTabsIntent = builder.build();
             CustomTabsHelper.addKeepAliveExtra(context, customTabsIntent.intent);
             customTabsIntent.launchUrl((Activity) context, Uri.parse(url));
+
+            /*Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            intent.putExtra(CustomTabsIntent.EXTRA_TITLE_VISIBILITY_STATE, CustomTabsIntent.SHOW_PAGE_TITLE);
+            CustomTabsHelper.addKeepAliveExtra(context, intent);
+
+            Bundle extras = new Bundle();
+            extras.putBinder(CustomTabsIntent.EXTRA_SESSION,
+                    mCustomTabsSession.asBinder()); // <-- asBinder is package local!
+            intent.putExtras(extras);
+
+            Bitmap icon = BitmapFactory.decodeResource(
+                    context.getResources(), R.drawable.ic_arrow_back_black_24dp);
+            intent.putExtra(CustomTabsIntent.EXTRA_CLOSE_BUTTON_ICON, icon);
+
+            Bundle finishBundle = ActivityOptions.makeCustomAnimation(
+                    context, R.anim.do_nothing, R.anim.activity_slide_out_to_right).toBundle();
+            intent.putExtra(CustomTabsIntent.EXTRA_EXIT_ANIMATION_BUNDLE, finishBundle);
+
+            Bundle startBundle = ActivityOptions.makeCustomAnimation(
+                    context, R.anim.activity_slide_in_from_right, R.anim.do_nothing).toBundle();
+
+            context.startActivity(intent, startBundle);*/
         } else {
             WPWebViewActivity.openURL(context, url);
         }
+    }
+
+    private static Boolean mIsCustomTabsSupported;
+    private static boolean isCustomTabsSupported(Context context) {
+        if (mIsCustomTabsSupported == null) {
+            String packageNameToBind = CustomTabsHelper.getPackageNameToUse(context);
+            if (packageNameToBind == null) {
+                mIsCustomTabsSupported = false;
+                return false;
+            }
+
+            CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
+                @Override
+                public void onCustomTabsServiceConnected(ComponentName name, CustomTabsClient client) {
+                }
+                @Override
+                public void onServiceDisconnected(ComponentName name) {
+                }
+            };
+
+            mIsCustomTabsSupported = CustomTabsClient.bindCustomTabsService(context, packageNameToBind, connection);
+            if (mIsCustomTabsSupported) {
+                context.unbindService(connection);
+            }
+        }
+
+        return mIsCustomTabsSupported;
     }
 }
