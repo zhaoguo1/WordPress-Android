c8be25f0a742059de804e06e18cbc172a2244448
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2016-06-14 01:54:35 +0300

    Simplify EndlistIndicatorViewModel

--- WordPress/src/main/java/org/wordpress/android/ui/posts/EndlistIndicatorViewModel.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/EndlistIndicatorViewModel.java
@@ -5,19 +5,17 @@ import org.wordpress.android.util.DisplayUtils;
 import android.content.Context;
 import android.databinding.BaseObservable;
 import android.databinding.BindingAdapter;
+import android.databinding.ObservableFloat;
+import android.databinding.ObservableInt;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class EndlistIndicatorViewModel extends BaseObservable {
-    private final int mEndlistIndicatorHeight;
+public class EndlistIndicatorViewModel {
+    public final ObservableFloat layoutHeight = new ObservableFloat();
 
     public EndlistIndicatorViewModel(Context context, boolean isPage) {
         // endlist indicator height is hard-coded here so that its horz line is in the middle of the fab
-        mEndlistIndicatorHeight = DisplayUtils.dpToPx(context, isPage ? 82 : 74);
-    }
-
-    public int getLayoutHeight() {
-        return mEndlistIndicatorHeight;
+        layoutHeight.set(DisplayUtils.dpToPx(context, isPage ? 82 : 74));
     }
 
     @BindingAdapter("android:layout_height")
