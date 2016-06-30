01a3e02b6ed8c4aada1538b881d466505a695dd3
Maxime Biais <maxime.biais@gmail.com>
2016-06-30 15:37:32 +0200

    replace wpstores subtree with jitpack build

--- libs/stores/WordPressStores/src/main/java/org/wordpress/android/stores/action/AuthenticationAction.java
+++
@@ -1,7 +0,0 @@
-package org.wordpress.android.stores.action;
-
-public enum AuthenticationAction implements IAction {
-    AUTHENTICATE,
-    AUTHENTICATE_ERROR,
-    UNAUTHORIZED,
-}
--- libs/stores/WordPressStores/src/main/java/org/wordpress/android/stores/action/IAction.java
+++
@@ -1,6 +0,0 @@
-package org.wordpress.android.stores.action;
-
-public interface IAction {
-    String name();
-    String toString();
-}
